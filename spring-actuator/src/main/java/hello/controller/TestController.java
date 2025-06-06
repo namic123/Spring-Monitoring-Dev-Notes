c13fdf984058706b3861283f5c5217a925307659
jspark <namic123@naver.com>
2025-06-07 01:58:33 +0900

    Grafana 모니터링 테스트. jvm_memory_used_bytes 메트릭 증가 테스트 OutOfMemoryError

--- spring-actuator/src/main/java/hello/controller/TestController.java
+++ spring-actuator/src/main/java/hello/controller/TestController.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @Slf4j
 public class TestController {
@@ -17,4 +20,16 @@ public class TestController {
         }
         return "ok value=" + value;
     }
+
+    private List<String> list = new ArrayList<>();
+
+    @GetMapping("/jvm")
+    public String jvm() {
+        log.info("jvm");
+        for (int i = 0; i < 1_000_000; i++) {
+            list.add("hello jvm!" + i);
+        }
+        return "ok";
+    }
+
 }
